9cb537adb6f5b0d84eac01d0064f66d39371cbd9
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2025-03-13 20:48:47 +0000

    Create traffic_generator.py

---
+++ traffic_generator.py
@@ -0,0 +1,17 @@
+
+
+
+
+def testing():
+    pass
+
+def main():
+    try:
+        while True:
+            #generate_traffic()
+    except KeyboardInterrupt:
+        print("[i] Traffic Generation Stopped.")
+
+
+if __name__ == '__main__':
+    testing()
